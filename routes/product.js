e761b3ae1715d55f4da74abbd705e6a68ffb5a75
Linh <khanhlinhdh.work@gmail.com>
2024-03-15 23:51:43 +0700

    create update function & delete function

--- routes/product.js
+++ routes/product.js
@@ -44,6 +44,42 @@ router.get('/delete/:id', async function(req, res, next) {
     await productModel.deleteOne({productID: req.params.id});
     res.redirect('/product');
 });
+
+/* GET update page */
+router.get('/update/:id', async function(req, res, next) {
+    let updateProduct = await productModel.findOne({productID: req.params.id});
+    res.render('product/update', {updateProduct});
+});
+
+/* POST update page */
+router.post('/updatePost/:id', upload.single('image'), async function(req, res, next) {
+    let file = req.file;
+    const body = req.body;
+    if (!file) {   
+        // Loại bỏ trường _id từ req.body
+        // delete req.body._id; 
+        // Sử dụng phương thức findOneAndUpdate để cập nhật tài liệu
+        await productModel.findOneAndUpdate({productID: req.params.id}, {$set: req.body});
+        res.redirect('/product');
+    }
+    else {
+        let updateData = {
+            productID: body.productID,
+            productName: body.productName,
+            productPrice: body.productPrice,
+            email: body.email,
+            password: body.password,
+            image: file.filename
+        };
+        // Loại bỏ trường _id từ đối tượng updateData
+        // delete updateData._id;
+        // Sử dụng phương thức findOneAndUpdate để cập nhật tài liệu
+        await productModel.findOneAndUpdate({productID: req.params.id}, {$set: updateData});
+        res.redirect('/product');
+    }
+});
+
+
 module.exports = router;
 
 
