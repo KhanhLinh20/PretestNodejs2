ed687225cae1ba751491b25310bd393db1982065
Linh <khanhlinhdh.work@gmail.com>
2024-03-16 12:02:43 +0700

    create register post method

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,27 @@ var express = require('express');
 const userModel = require('../Model/userModel');
 var router = express.Router();
 
+const bcryptjs = require('bcryptjs');
+
 /* GET register page */
 router.get('/register', async function(req, res) {
     res.render('user/register');
 })
+
+/* POST register page */
+router.post('/registerPost', async function(req, res, next) {
+    // Hash password
+    const hashedPassword = await bcryptjs.hash(req.body.passwordInput, 10);
+    const hashedConfirmPassword = await bcryptjs.hash(req.body.confirmPasswordInput, 10);
+
+    let userU = new userModel({
+        name: req.body.nameInput,
+        phone: req.body.phoneNumber,
+        email: req.body.emailInput,
+        password: hashedPassword,
+        confirmPassword: hashedConfirmPassword,
+    });
+    await userU.save();
+    res.redirect('/product');
+});
 module.exports = router;
